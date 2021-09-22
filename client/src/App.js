5b58930fef093cc456f13ee6a40d78681bbeac74
jjl5508 <84102321+jjl5508@users.noreply.github.com>
2021-09-22 14:55:49 -0700

    Completed EditUser/Routing for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import EditUser from './views/EditUser';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = "/users/new">
+            <NewUser />
+          </Route>
+
+          <Route exact path = "/users/:id">
+            <User />
+          </Route>
+
+          <Route exact path = "/users/:id/edit">
+            <EditUser />
+          </Route>
+          
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/views/EditUser.js
+++ client/src/views/EditUser.js
@@ -15,4 +15,51 @@ const EditUser = (props) => {
 
     const [errors, setErrors] = useState(null);
 
+    const history = useHistory();
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/users/" + id)
+            .then((res) => {
+                console.log(res.data);
+                setfirstName(res.data.firstName);
+                setlastName(res.data.lastName);
+                setEmail(res.data.email);
+                setPassword(res.data.password);
+                setGender(res.data.gender);
+                setAge(res.data.age);
+                setSrc(res.data.src);
+                setDescription(res.data.description);
+            })
+            .catch((err) => {
+                console.log(err.response);
+            });
+    }, [id]);
+
+    const handleEditUserSubmit = (e) => {
+        e.preventDefault();
+
+        const editedUser = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            gender: gender,
+            age: age,
+            src: src,
+            description: description,
+        };
+
+        axios
+            .put("http://localhost:8000/api/users" + id, editedUser)
+            .then((res) => {
+                console.log(res.data);
+                history.push(`users`);
+            })
+            .catch((err) => {
+                setErrors(err.response.data.errors);
+            });
+    };
 }
+
+export default EditUser;
