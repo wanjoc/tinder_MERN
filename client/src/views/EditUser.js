e444fbc2ae29ec0020a7606c2a0fbc672b6f26fb
jjl5508 <84102321+jjl5508@users.noreply.github.com>
2021-09-22 13:34:50 -0700

    edit user/ bcrypt install

---
+++ client/src/views/EditUser.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+
+const EditUser = (props) => {
+    const { id } = useParams();
+
+    const [firstName, setfirstName] = useState("");
+    const [last]
+}
