08f44e94c44bd4b6c8a6cbb814c07d3f8617dbc8
jjl5508 <84102321+jjl5508@users.noreply.github.com>
2021-09-23 16:15:30 -0700

    Updated Dashboard for log out functionality

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -2,19 +2,21 @@ import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Dashboard = (props) => {
+const Dashboard = () => {
     const history = useHistory();
 
-    const handleLogOut = (e) => {
-        axios.get("http://localhost:8000/api/users/logout")
-            .then((res) => { console.log(res.data); history.push('/users/login'); })
-            .catch((err) => { console.log(err.response.data.errors); });
+    const handleLogOut = () => {
+        sessionStorage.setItem("usertoken", '');
+        sessionStorage.clear();
+        history.push("/users/login"); // whichever component you want it to route to
+    }
 
-    };
     return (
         <div>
-            <h1>Success</h1>
-            <button type="button" onClick={handleLogOut}>Log Out</button>
+            <h1> Success! </h1>
+            <button type="button" onClick={handleLogOut}>
+                Go home
+            </button>
         </div>
     );
 }
