a7455ba3286e722dea807f0f1962999e1654eac0
jjl5508 <84102321+jjl5508@users.noreply.github.com>
2021-09-23 14:22:59 -0700

    Update EditUser.js

--- client/src/views/EditUser.js
+++ client/src/views/EditUser.js
@@ -5,13 +5,13 @@ import axios from 'axios';
 const EditUser = (props) => {
     const { id } = useParams();
 
-    const [firstName, setfirstName] = useState("");
-    const [lastName, setlastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [gender, setGender] = useState("");
     const [age, setAge] = useState("");
-    const [src, setSrc] = useState("");
+    const [src, setSrcImage] = useState("");
     const [description, setDescription] = useState("");
 
     const [errors, setErrors] = useState(null);
@@ -23,13 +23,13 @@ const EditUser = (props) => {
             .get("http://localhost:8000/api/users/" + id)
             .then((res) => {
                 console.log(res.data);
-                setfirstName(res.data.firstName);
-                setlastName(res.data.lastName);
+                setFirstName(res.data.firstName);
+                setLastName(res.data.lastName);
                 setEmail(res.data.email);
                 setPassword(res.data.password);
                 setGender(res.data.gender);
                 setAge(res.data.age);
-                setSrc(res.data.src);
+                setSrcImage(res.data.src);
                 setDescription(res.data.description);
             })
             .catch((err) => {
@@ -61,6 +61,84 @@ const EditUser = (props) => {
                 setErrors(err.response.data.errors);
             });
     };
+    return (
+        <div className="container p-5">
+            <div style={{ width: "570px", height: "auto", border: "1px solid #eee", padding: "50px", margin: "0 auto" }}>
+                <form onSubmit={handleEditUserSubmit}>
+                    <h1 style = {{ textAlign: "center", marginBottom: "15px"}}>Edit User</h1>
+
+                    <div className="d-flex justify-content-between">
+                        <div className="mb-3">
+                            <input type="text" className="form-control" placeholder="First Name" onChange={e => { setFirstName(e.target.value) }}></input>
+                            {errors?.firstName && (
+                                <span style={{ color: "red" }}> {errors?.firstName?.message}</span>
+                            )}
+                        </div>
+                        <div className="mb-3">
+                            <input type="text" className="form-control" placeholder="Last Name" onChange={e => { setLastName(e.target.value) }}></input>
+                            {errors?.lastName && (
+                                <span style={{ color: "red" }}> {errors?.lastName?.message}</span>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="d-flex justify-content-between">
+                        <div className="mb-3 me-5">
+                            <input type="number" className="form-control" placeholder="Age" onChange={e => { setAge(e.target.value) }}></input>
+                            {errors?.age && (
+                                <span style={{ color: "red" }}> {errors?.age?.message}</span>
+                            )}
+                        </div>
+                        <div className="mb-3">
+                            <input type="text" className="form-control" placeholder="Email" onChange={e => { setEmail(e.target.value) }}></input>
+                            {errors?.email && (
+                                <span style={{ color: "red" }}> {errors?.email?.message}</span>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="me-2">Gender: </label>
+                        <select onChange={e => { setGender(e.target.value) }} value={gender}>
+                            <option value="Male" >Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Prefer not to answer, or other">Prefer not to answer, or other</option>
+                        </select>
+                        {errors?.gender && (
+                            <span style={{ color: "red" }}> {errors?.gender?.message}</span>
+                        )}
+                    </div>
+
+                    <div className="d-flex justify-content-between">
+                        <div className="mb-3">
+                            <input type="password" className="form-control" placeholder="Password" onChange={e => { setPassword(e.target.value) }}></input>
+                            {errors?.password && (
+                                <span style={{ color: "red" }}> {errors?.password?.message}</span>
+                            )}
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" className="form-control" placeholder="Image url" onChange={e => { setSrcImage(e.target.value) }}></input>
+                        {errors?.src && (
+                            <span style={{ color: "red" }}> {errors?.src?.message}</span>
+                        )}
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">About me: </label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" onChange={e => { setDescription(e.target.value) }}></textarea>
+                        {errors?.description && (
+                            <span style={{ color: "red" }}> {errors?.description?.message}</span>
+                        )}
+                    </div>
+
+                    <div className="d-flex">
+                        <button className="btn-sm btn-primary me-5" type="submit">Submit!</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default EditUser;
