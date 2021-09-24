5cc5a33c0dec2c7f2cc801174c929e472d97feae
jjl5508 <84102321+jjl5508@users.noreply.github.com>
2021-09-24 11:21:56 -0700

    push to pull

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -8,7 +8,7 @@ Export a function to be called in server.js where the app will be passed in.
 
 module.exports = app => {
     //returns a list of all users
-    app.get("/api/users", authenticate, UserController.findAllUsers);
+    app.get("/api/users", UserController.findAllUsers);
 
     //returns a user searched by id
     app.get("/api/users/:id",UserController.findOneUser);
