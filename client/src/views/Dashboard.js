d92c9a708ce816b83a8a741c26789549f377f722
wanjoc <85894801+wanjoc@users.noreply.github.com>
2021-09-24 11:41:59 -0700

    test

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,27 +1,24 @@
+import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Dashboard = (props) => {
-const Dashboard = () => {
     const history = useHistory();
 
-    const handleLogOut = () => {
-        sessionStorage.setItem("usertoken", '');
-        sessionStorage.clear();
-        history.push("/users/login"); // whichever component you want it to route to
-    }
-<<<<<<< HEAD
-//hel
-=======
+    const handleLogOut = (e) => {
+        axios.post("http://localhost:8000/api/users/logout")
+            .then(history.push("/users/login"));
 
     };
->>>>>>> d743ae4f646f56de35a3ee8754df90cd6d21586b
     return (
         <div>
-            <h1> Success! </h1>
-            <button type="button" onClick={handleLogOut}>
-                Go home
-            </button>
+            <h1>Success</h1>
+            <form onSubmit={handleLogOut}>
+                <button type="submit">Log Out</button>
+            </form>
+            
         </div>
     );
 }
+
+export default Dashboard;
